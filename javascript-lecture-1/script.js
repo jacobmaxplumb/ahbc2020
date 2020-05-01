1067516546c11ac4564b62a7ed7b4de870e0592c
Jacob Plumb <jacobmaxplumb@gmail.com>
2020-04-30 21:04:49 -0400

    javascript exercise in class added

---
+++ javascript-lecture-1/script.js
@@ -0,0 +1,8 @@
+const string1 = 'string1';
+let string2 = 'string2';
+const number1 = 1;
+const nullValue = null;
+let undValue;
+string2 = 'string3';
+
+console.log(`string1: ${string1}, string2: ${string2}, number1: ${number1}, nullValue: ${nullValue}, undValue: ${undValue}`)
