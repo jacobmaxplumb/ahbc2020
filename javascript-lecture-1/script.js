0cb213cd726448faec4aa13b542a339b46eb26c0
Jacob Plumb <jacobmaxplumb@gmail.com>
2020-05-04 18:31:21 -0400

    uploaded first javascript lectuce example

--- javascript-lecture-1/script.js
+++ javascript-lecture-1/script.js
@@ -1,8 +1,8 @@
-const string1 = 'string1';
-let string2 = 'string2';
+let string1 = 'string1';
+let string2 = string1;
+string1 = 'another value';
 const number1 = 1;
 const nullValue = null;
 let undValue;
-string2 = 'string3';
 
 console.log(`string1: ${string1}, string2: ${string2}, number1: ${number1}, nullValue: ${nullValue}, undValue: ${undValue}`)
