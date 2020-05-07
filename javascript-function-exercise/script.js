de6824f16f8a3a44c1299f9d91bad46bf32f4c38
Jacob Plumb <jacobmaxplumb@gmail.com>
2020-05-07 08:34:35 -0400

    uploaded javascript function exercise

---
+++ javascript-function-exercise/script.js
@@ -0,0 +1,15 @@
+function getAreaOfCircle(radius) {
+    return Math.PI * radius * radius;
+}
+
+function getCircumferenceOfCircle(radius) {
+    return 2 * Math.PI * radius;
+}
+
+function getAreaOfSquare(side) {
+    return side * side;
+}
+
+function getAreaOfTriangle(base, height) {
+    return (base * height) / 2;
+}
