eda9fdf676abc80a108ea18829da7f4c32b58eab
Jacob Plumb <jacobmaxplumb@gmail.com>
2020-05-27 21:38:38 -0400

    added dom lecture 1

---
+++ jacobs-solutions/dom-lecture-1/script.js
@@ -0,0 +1,31 @@
+//let selectedDiv = document.getElementById('jacobs-div');
+// let selectedDiv = document.querySelector('#jacobs-div');
+// console.log(selectedDiv);
+
+// let ptags = document.querySelectorAll('p');
+// console.log(ptags);
+
+//document.getElementsByClassName('this-class')
+// document.querySelector('.this-class');
+
+// let element = document.getElementById('jacobs-div');
+// element.classList.toggle('highlight');
+// if (element.classList.contains("something1")) {
+//     element.classList.toggle('highlight');
+// }
+
+// let linkElement = document.getElementById('my-link');
+// console.log(linkElement.getAttribute('href'));
+
+// linkElement.setAttribute('href', 'https://google.com');
+// console.log(linkElement.getAttribute('href'));
+// element.innerHTML = 'something else';
+// element.classList.add('highlight');
+// console.log(element);
+
+// document.getElementById('my-link').setAttribute('href', 'https://google.com');
+
+const element1 = document.getElementById('jacobs-div-1');
+const element2 = document.getElementById('jacobs-div-2');
+element1.innerHTML = "<h1>Title</h1>";
+element2.innerText = "<h1>Title</h1>";
