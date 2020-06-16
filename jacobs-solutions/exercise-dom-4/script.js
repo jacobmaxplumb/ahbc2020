6403c823ca0dd93b7657bdf6950557f4fdcad827
Nicole <nbush89@gmail.com>
2020-06-16 09:40:04 -0400

    added DOM in-class exercise

--- jacobs-solutions/exercise-dom-4/script.js
+++ jacobs-solutions/exercise-dom-4/script.js
@@ -1,24 +1,26 @@
-const moneyForm = document.getElementById('money-form');
-const coinsArea = document.getElementById('coins-area');
+const moneyForm = document.getElementById("money-form");
+const coinsArea = document.getElementById("coins-area");
 
 console.log(moneyForm);
+// function addMoney()
 
-moneyForm.addEventListener('click', (e) => {
-    console.log('something');
-    e.preventDefault();
+moneyForm.addEventListener("submit", (e) => {
+  console.log(e);
+  e.preventDefault();
 
-    let data = new FormData(moneyForm);
+  let data = new FormData(moneyForm);
 
-    let coinCount = parseInt(data.get('count'));
+  let coinCount = parseInt(data.get("count"));
 
-    for (let i = 0; i < coinCount; i++) {
-        let coin = document.createElement('div');
-        coin.classList.add('coin');
-        coin.innerText = data.get('coin');
-        coinsArea.appendChild(coin);
-    }
+  for (let i = 0; i < coinCount; i++) {
+    let coin = document.createElement("div");
+    coin.classList.add("coin");
+    coin.innerText = data.get("coin");
+    coinsArea.append(coin);
+  }
 });
 
-coinsArea.addEventListener('click', e => {
-    e.target.remove();
-})
+coinsArea.addEventListener("click", (e) => {
+  console.log(e);
+  e.target.remove();
+});
