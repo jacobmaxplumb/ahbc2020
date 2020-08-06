1c728a7449d36ad6751a2ae4f7ed894d4cc42104
Nicole <nbush89@gmail.com>
2020-08-05 20:46:56 -0400

    small change

--- nicoles-solutions/node/node-lab/quotes.js
+++ nicoles-solutions/node/node-lab/quotes.js
@@ -2,7 +2,7 @@ let quotes = [
   "Never go to bed mad. Stay up and fight.",
   "When life gives you lemons, squirt someone in the eye.",
   "If a book about failures doesn't sell, is it a success?",
-  "According to most studies, peoples number one fear is public speaking. Number two is death. Death is number two. Does that seem right? That means to the average person, if you have to go to a funeral, youre better off in the casket than doing the eulogy.",
+  "According to most studies, people’s number one fear is public speaking. Number two is death. Death is number two. Does that seem right? That means to the average person, if you have to go to a funeral, you’re better off in the casket than doing the eulogy.",
   "Not sure which is harder on a relationship: sharing a dresser for three years or sharing an iPhone charger for one day.",
   "Whoever established the high road and how high it should be should be fired.",
 ];
